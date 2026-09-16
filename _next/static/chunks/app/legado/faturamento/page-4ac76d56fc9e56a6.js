833bea99e9514bfb3a592783d60bf6c8548fbfee
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-16 15:30:50 -0300

    deploy: payables visibility for all adm/finance, user management, and conference redesign

---
+++ _next/static/chunks/app/legado/faturamento/page-4ac76d56fc9e56a6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9905],{87877:function(n,e,u){Promise.resolve().then(u.bind(u,49410))}},function(n){n.O(0,[4358,9990,9735,4851,1721,3335,5428,2135,9088,8825,2971,2117,1744],function(){return n(n.s=87877)}),_N_E=n.O()}]);
