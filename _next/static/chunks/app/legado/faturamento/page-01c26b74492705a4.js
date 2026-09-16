867ea5e5b9452966df8bd7f592ff97319e7443ab
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-16 12:17:45 -0300

    deploy: update gh-pages with redesigned employee modal

--- _next/static/chunks/app/legado/faturamento/page-01c26b74492705a4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9905],{53395:function(n,e,u){Promise.resolve().then(u.bind(u,49410))}},function(n){n.O(0,[4358,9990,9735,1165,8317,3335,5428,2135,8306,9410,2971,2117,1744],function(){return n(n.s=53395)}),_N_E=n.O()}]);
