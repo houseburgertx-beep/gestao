7bb8c56c956ba408f37d3fb75d9d509d14c20812
houseburgertx-beep <houseburgertx-beep@users.noreply.github.com>
2026-09-16 10:05:16 -0300

    deploy: publica novo seletor minimalista de datas e cabecalho

---
+++ _next/static/chunks/app/faturamento/page-01a176ede8c305f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1546],{6790:function(n,e,u){Promise.resolve().then(u.bind(u,3859))},3859:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return t.default}});var t=u(49410)}},function(n){n.O(0,[4358,9990,9735,1165,1721,3335,5428,2135,6966,9410,2971,2117,1744],function(){return n(n.s=6790)}),_N_E=n.O()}]);
