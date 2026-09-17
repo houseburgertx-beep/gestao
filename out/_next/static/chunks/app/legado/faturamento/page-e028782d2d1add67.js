1f6f0ec0ac208ac6f7f95f39191825b8c84cb393
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-16 23:09:32 -0300

    deploy: clean static export, remove .next and build artifacts

--- out/_next/static/chunks/app/legado/faturamento/page-e028782d2d1add67.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9905],{41335:function(n,e,u){Promise.resolve().then(u.bind(u,93177))}},function(n){n.O(0,[8106,4384,3820,2501,3047,1638,7780,9928,2115,4087,7115,2364,1744],function(){return n(n.s=41335)}),_N_E=n.O()}]);
