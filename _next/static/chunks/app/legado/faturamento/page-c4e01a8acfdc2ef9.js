ebcd668af24066688917e78e969a5f422233f251
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-18 01:09:14 -0300

    deploy: fix excluir fechamento - updatedBy usuario atual ao arquivar

--- _next/static/chunks/app/legado/faturamento/page-c4e01a8acfdc2ef9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9905],{87877:function(n,e,u){Promise.resolve().then(u.bind(u,42367))}},function(n){n.O(0,[4358,9990,9735,9881,9240,3335,5445,2135,9088,2964,2971,2117,1744],function(){return n(n.s=87877)}),_N_E=n.O()}]);
