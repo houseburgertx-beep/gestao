372a468686d6edb223d089d8e781810cee6a1585
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-19 20:19:57 -0300

    deploy: aba de auditorias para operadoras e cards de auditoria conforme fotos 1 e 2

---
+++ _next/static/chunks/app/fechamento-caixa/page-efd697d81427ab62.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6172],{},function(n){n.O(0,[3042,8106,4384,3820,5478,1284,9928,5807,7115,2364,1744],function(){return n(n.s=85807)}),_N_E=n.O()}]);
