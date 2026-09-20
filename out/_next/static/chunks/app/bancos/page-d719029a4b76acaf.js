f6a79050ea7671fabda6f1780ee30523ad0e8b18
GLEUCE DIAS DE SOUZA <gleuce@MacBook-Air-de-GLEUCE.local>
2026-09-20 10:46:53 -0300

    deploy: permissoes de operador para reabrir e editar fechamento de caixa e pix

--- out/_next/static/chunks/app/bancos/page-d719029a4b76acaf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7415],{32523:function(n,e,u){Promise.resolve().then(u.bind(u,84592))}},function(n){n.O(0,[3042,8106,4384,3820,8837,1284,9928,4592,7115,2364,1744],function(){return n(n.s=32523)}),_N_E=n.O()}]);
